f31e8bbcb36d482188fc3038c8d38f260bec5ea5
JulianSchu <jun.xu@hotmail.dem>
2019-04-29 17:57:17 +0200

    search added

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 let vm = new Vue({
     el: '#books',
     data: {
-        books: []
+        books: [],
+        search: ''
     },
     methods: {
         fetchData() {
             fetch('https://api.myjson.com/bins/udbm5')
-            .then (res => res.json())
-            .then (data => {
-                this.books = data.books;
-                console.log(this.books)
-                
-            })
-            .catch(err => console.log(err))
+                .then(res => res.json())
+                .then(data => {
+                    this.books = data.books;
+                    console.log(this.books)
+
+                })
+                .catch(err => console.log(err))
         }
+    },
+    computed: {
+        filteredBooks: function () {
+            let str = this.search;
+            return this.books.filter(function (book) {
+                    if (book.titulo.match(str) || book.descripcion.match(str) || book.idioma.match(str)) return true;
+                })
+            }
     },
     mounted() {
         this.fetchData();
     }
 })
-
