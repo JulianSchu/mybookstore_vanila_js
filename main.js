63d36965b8d98a85c4e7eda62a6de9d4d8d32ddd
JulianSchu <jun.xu@hotmail.dem>
2019-04-29 14:53:28 +0200

    initialize bookstore

---
+++ main.js
@@ -0,0 +1,21 @@
+let vm = new Vue({
+    el: '#books',
+    data: {
+        books: []
+    },
+    methods: {
+        fetchData() {
+            fetch('https://api.myjson.com/bins/udbm5')
+            .then (res => res.json())
+            .then (data => {
+                this.books = data.books;
+                console.log(this.books)
+                
+            })
+            .catch(err => console.log(err))
+        }
+    },
+    mounted() {
+        this.fetchData();
+    }
+})
