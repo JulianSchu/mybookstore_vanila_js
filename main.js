e083c9c5cee28afe4e94523dcb1c1d0add3cb343
JulianSchu <jun.xu@hotmail.dem>
2019-04-30 13:02:00 +0200

    bookstore done

--- main.js
+++ main.js
@@ -2,27 +2,44 @@ let vm = new Vue({
     el: '#books',
     data: {
         books: [],
-        search: ''
+        search: '',
+        footer: false
     },
     methods: {
         fetchData() {
             fetch('https://api.myjson.com/bins/udbm5')
                 .then(res => res.json())
                 .then(data => {
-                    this.books = data.books;
+                let books = data.books;
+                books.forEach(function(each){
+                    if (books.indexOf(each) === 0) {
+                        each.carousel_class = 'carousel-item active'
+                    } else {
+                        each.carousel_class = 'carousel-item'
+                    }
+                })
+                    this.books = books;
                     console.log(this.books)
 
                 })
                 .catch(err => console.log(err))
+        },
+        nullResult(){
+            let result = this.filteredBooks;
+            if (result.length === 0) {
+                this.footer = true;
+            } else {
+                this.footer = false;
+            }
         }
     },
     computed: {
         filteredBooks: function () {
             let str = this.search;
             return this.books.filter(function (book) {
-                    if (book.titulo.match(str) || book.descripcion.match(str) || book.idioma.match(str)) return true;
-                })
-            }
+                if (book.titulo.match(str) || book.descripcion.match(str) || book.idioma.match(str)) return true;
+            })
+        }
     },
     mounted() {
         this.fetchData();
